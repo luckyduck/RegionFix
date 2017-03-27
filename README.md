f03aefa68be5950fc3efea86cc3d90cdad86adbb
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-03-27 18:47:12 +0200

    First Version

---
+++ README.md
@@ -0,0 +1,4 @@
+### Regionfix for Magento2
+
+Provides a fix for following issue:
+https://github.com/magento/magento2/issues/6261
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Twinsen_RegionFix',
+    __DIR__
+);
